ce273cb7bdbf7a0832c260c9025f1e9b19830c66
MD Aminul Islam <aminulsc12@gmail.com>
2022-11-26 18:52:05 +0600

    confirmation modal create

--- src/Components/Pages/Dashboard/AllBuyers/AllBuyers.js
+++ src/Components/Pages/Dashboard/AllBuyers/AllBuyers.js
@@ -1,10 +1,10 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useQuery } from '@tanstack/react-query'
 import useAdmin from '../../../hooks/useAdmin';
 import { AuthContext } from '../../../Context/AuthProvider';
 
-import ConfirmModal from '../../../Shared/ConfermModal/ConfirmModal';
 const AllBuyers = () => {
+    const [buyerDelete, setBuyerDelete] = useState(null)
     const { user } = useContext(AuthContext)
     const [isAdmin] = useAdmin(user?.email)
     const { data: users = [], refetch } = useQuery({
@@ -50,13 +50,13 @@ const AllBuyers = () => {
                     {
                         usersinfo.map((userinfo, i) =>
                              <tr key={userinfo._id} className="hover">
-                                <th>{i + 1}</th>
+                                <th>{i+1}</th>
                                 <td>{userinfo.name}</td>
                                 <td>{userinfo.email}</td>
                                 <td>{userinfo.option}</td>
                                 <td>
                                     {
-                                        user && isAdmin && <> <label htmlFor="delete-modal" className="btn btn-error text-base-100">Delete</label></>
+                                        user && isAdmin && <> <label onClick={()=> setBuyerDelete(userinfo)} htmlFor="delete-modal" className="btn btn-error text-base-100">Delete</label></>
                                     }
                                 </td>
                             </tr>
@@ -64,10 +64,22 @@ const AllBuyers = () => {
                     }
                 </tbody>
             </table>
-            <ConfirmModal
-                handlerDeleteBuyer={handlerDeleteBuyer}
-                usersinfo = {usersinfo}
-            ></ConfirmModal>
+            {
+                buyerDelete &&
+                    <div>
+                        <input type="checkbox" id="delete-modal" className="modal-toggle" />
+                        <div className="modal">
+                            <div className="modal-box">
+                                <h3 className="font-extrabold text-lg">Are you sure to delete {buyerDelete.name}?</h3>
+                                <p className="py-4"></p>
+                                <div className="modal-action">
+                                    <label htmlFor="delete-modal" onClick={()=> handlerDeleteBuyer(buyerDelete._id)} className='btn btn-error text-base-100'>Delete</label>
+                                    <label htmlFor="delete-modal" className="btn btn-outline">Cancel</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            }
         </div>
     );
 };
--- src/Components/Pages/Dashboard/AllSallers/AllSallers.js
+++ src/Components/Pages/Dashboard/AllSallers/AllSallers.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { useQuery } from '@tanstack/react-query'
+import useAdmin from '../../../hooks/useAdmin';
+import { AuthContext } from '../../../Context/AuthProvider';
 
 
 
 const AllSallers = () => {
-    const { data: users = [] } = useQuery({
+    const { user } = useContext(AuthContext)
+    const [isAdmin] = useAdmin(user?.email)
+    const [sallerDelete, setSallerDelete] = useState(null)
+    const { data: users = [], refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await fetch(`http://localhost:5000/users`);
@@ -14,8 +19,21 @@ const AllSallers = () => {
     })
 
     const usersinfo = users.filter(user=> user.option === 'Seller')
+
     
-    
+    const handlerDeleteSaller = id => {
+        console.log(id);
+        fetch(`http://localhost:5000/users/${id}`, {
+            method: 'DELETE',
+        }).then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    
+                    refetch()
+                }
+            })
+    }
+
     return (
         <div className="overflow-x-auto ml-4">
             <table className="table w-full">
@@ -27,19 +45,25 @@ const AllSallers = () => {
                         <th>Email</th>
                         <th>Option</th>
                         <th>Verification</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     {
                         usersinfo.map((userinfo, i)=>
-                        <tr className="hover">
+                        <tr key={userinfo._id} className="hover">
                             <th>{i+1}</th>
                             <td>{userinfo.name}</td>
                             <td>{userinfo.email}</td>
                             <td>{userinfo.option}</td>
                             <td>
                                 {
-                                    userinfo?.verification ? <button className='btn btn-success'>Verified</button> : <button className='btn btn-success'>Unverified</button>
+                                   user && isAdmin && <button className='btn btn-success text-base-100'>Verify</button> 
+                                }
+                            </td>
+                            <td>
+                                {
+                                    user && isAdmin && <label onClick={()=> setSallerDelete(userinfo)} htmlFor='delete-modal' className='btn btn-error text-base-100'>Delete</label> 
                                 }
                             </td>
                         </tr>  
@@ -47,6 +71,22 @@ const AllSallers = () => {
                     }
                 </tbody>
             </table>
+            {
+                sallerDelete && 
+                    <div>
+                        <input type="checkbox" id="delete-modal" className="modal-toggle" />
+                        <div className="modal">
+                            <div className="modal-box">
+                                <h3 className="font-extrabold text-lg">Are you sure to delete {sallerDelete.name}?</h3>
+                                
+                                <div className="modal-action">
+                                    <label htmlFor="delete-modal" onClick={()=> handlerDeleteSaller(sallerDelete._id)} className='btn btn-error text-base-100'>Delete</label>
+                                    <label htmlFor="delete-modal" className="btn btn-outline">Cancel</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            }
         </div>
     );
 };
